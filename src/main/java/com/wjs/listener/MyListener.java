ecaf527db9f40ff5129198e9f352fbff7182ea00
wjs <1476105441@qq.com>
2022-02-01 01:14:00 +0800

    first commit

---
+++ src/main/java/com/wjs/listener/MyListener.java
@@ -0,0 +1,18 @@
+package com.wjs.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class MyListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("服务启动");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("服务关闭");
+    }
+}
